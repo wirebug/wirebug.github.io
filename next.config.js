22b0715308539a5c0c95505a855b51918f08926f
Christoph Weinzierl <christoph.weinzierl@adesso.de>
2022-01-23 16:19:23 +0100

    feat: add cd pipeline for gh pages
    s

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: isProd ? "/wirebug.github.io/" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
