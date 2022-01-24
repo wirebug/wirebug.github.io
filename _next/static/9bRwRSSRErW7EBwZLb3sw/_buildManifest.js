5a8788cccd59daa19a0a7a49556d5ef2a53552cf
wirebug <wirebug@users.noreply.github.com>
2022-01-24 10:22:04 +0000

    deploy: dc3f773dee2ca1be2e7b70c3d9939993f4e67a47

---
+++ _next/static/9bRwRSSRErW7EBwZLb3sw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/962-5587bc91757d89ab.js","static/css/39f4d8aba4b66723.css","static/chunks/pages/index-03b97e3fbf0f90d5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
